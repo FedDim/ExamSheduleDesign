858ddf05c067f642e70d6eed37b6a05c4e32bdb3
Дмитрий <146709037+FedDim@users.noreply.github.com>
2026-05-31 05:18:42 +0300

    refacfor: F acrylic

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,7 +1,5 @@
-﻿using ExamSheduleDesign.Helpers;
-using ExamSheduleDesign.ViewModels;
+﻿using ExamSheduleDesign.ViewModels;
 using MahApps.Metro.Controls;
-using System.Windows;
 
 namespace ExamSheduleDesign
 {
@@ -11,31 +9,6 @@ namespace ExamSheduleDesign
         {
             InitializeComponent();
             this.DataContext = new MainWindowViewModel();
-            WindowAcrylicHelper.EnableAcrylicBlur(this, 0x19FFFFFF);
-
-            // Подписываемся на изменение состояния окна
-            this.StateChanged += MainWindow_StateChanged;
-            // Устанавливаем начальное скругление
-            UpdateCornerRadius();
-        }
-
-        private void MainWindow_StateChanged(object sender, System.EventArgs e)
-        {
-            UpdateCornerRadius();
-        }
-
-        private void UpdateCornerRadius()
-        {
-            if (WindowState == WindowState.Maximized)
-            {
-                // При максимизации убираем скругления
-                ContentBorder.CornerRadius = new CornerRadius(0);
-            }
-            else
-            {
-                // В обычном режиме возвращаем скругления (только левая верхняя и левая нижняя)
-                ContentBorder.CornerRadius = new CornerRadius(12, 0, 0, 12);
-            }
         }
     }
 }
