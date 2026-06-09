b0c1732a568bc7d635ad2b30427ea9e52895e8c1
FedDim <146709037+FedDim@users.noreply.github.com>
2026-06-09 10:12:27 +0300

    feat: teacher name converter

---
+++ Converters/TeacherNameNormalizingConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ExamSheduleDesign.Converters
+{
+    public class TeacherNameNormalizingConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string name && !string.IsNullOrWhiteSpace(name))
+            {
+                // Приводим к нижнему регистру, затем делаем первую букву каждого слова заглавной
+                var textInfo = CultureInfo.CurrentCulture.TextInfo;
+                var lower = name.ToLower();
+                return textInfo.ToTitleCase(lower);
+            }
+            return value ?? "";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
